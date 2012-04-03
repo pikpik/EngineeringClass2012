14d65e6fd4f71b5a1f1c04fac312188a41acb5ff
pikpik <pikpik.1010@gmail.com>
2012-04-03 01:04:59 -0400

    I added code for seeing SparkFun 5DOF sensor tilt responses.

---
+++ HelicopterProject/arduino-code/Listen_to_5DOF_Sensor/5DOF_sensor_readings.ino
@@ -0,0 +1,21 @@
+int x, y;  // X and Y in-plane sensing
+
+void setup () {
+  
+  Serial.begin ( 9600 );      // sets the serial port to 9600
+  
+}
+
+
+void loop () {
+  
+  x = analogRead(0);       // read analog input pin 0
+  y = analogRead(1);       // read analog input pin 1
+  
+  Serial.print(x, DEC);    // print the rotational rate in the X axis
+  Serial.print(",");       // prints a space between the numbers
+  Serial.println(y, DEC);  // print the rotational rate in the Y axis
+  
+  delay(100);              // wait 100ms for next reading
+  
+}
